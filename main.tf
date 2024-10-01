16167307755795afacd0eb2855134767788ff52e
Manuel Nuila <manuel.nuila@cuemby.com>
2024-09-30 23:45:47 -0600

    first commit RabbitMQ

--- main.tf
+++ main.tf
@@ -2,8 +2,27 @@ locals {
   context = var.context
 }
 
-module "submodule" {
-  source = "./modules/submodule"
+resource "kubernetes_namespace" "rabbit_mq" {
+  metadata {
+    name = var.namespace_name
+  }
+}
 
-  message = "Hello, submodule"
+resource "helm_release" "rabbit_mq" {
+  name       = var.release_name
+  repository = "oci://registry-1.docker.io/bitnamicharts/rabbitmq"
+  chart      = "rabbitmq"
+  version    = var.chart_version
+  namespace  = var.namespace_name
+
+  # Ingress values
+  values = [
+    templatefile("${path.module}/values.yaml.tpl", {
+      username         = var.username,
+      password         = var.password,
+      domain_name      = var.domain_name,
+      issuer_name      = var.issuer_name
+      issuer_kind      = var.issuer_kind
+    })
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -23,3 +23,58 @@ EOF
   type        = map(any)
   default     = {}
 }
+
+#
+# RabbitMQ variables
+#
+
+variable "release_name" {
+  description = "The name of the Helm release."
+  type        = string
+  default     = "rabbitmq"
+}
+
+variable "namespace_name" {
+  description = "The namespace where the Helm release will be installed."
+  type        = string
+  default     = "rabbit-mq"
+}
+
+variable "chart_version" {
+  description = "The version of the ingress-nginx Helm chart."
+  type        = string
+  default     = "15.0.1"
+}
+
+#
+# RabbitMQ manifest variables
+#
+
+variable "username" {
+  type        = string
+  description = "Username for RabbitMQ"
+}
+
+variable "password" {
+  type        = string
+  description = "Password for RabbitMQ"
+  sensitive   = true 
+}
+
+variable "domain_name" {
+  type        = string
+  description = "domain name for RabbitMQ, e.g. 'dev.domainname.com'"
+  default     = "dev.domainname.com"
+}
+
+variable "issuer_name" {
+  type        = string
+  description = "origin issuer name"
+  default     = "origin-ca-issuer"
+}
+
+variable "issuer_kind" {
+  type        = string
+  description = "origin issuer kind"
+  default     = "ClusterOriginIssuer"
+}
